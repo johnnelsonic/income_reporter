f5e94113e6cbd3f2da1ef1db1f74ce93ea5164ae
John Nelson <john.nelson@blackbaud.com>
2017-05-07 08:16:56 -0500

    first commit

---
+++ src/main/java/com/johnnelsonic/IncomeReporterApplication.java
@@ -0,0 +1,12 @@
+package com.johnnelsonic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomeReporterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncomeReporterApplication.class, args);
+	}
+}
